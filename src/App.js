4e899e8f1f887e54ef012961017c075f43ce978d
flare-s <webflarez@gmail.com>
2022-08-04 15:45:49 +0100

    Added the app header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>Expenses Tracker</h1>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   );
 }
 
