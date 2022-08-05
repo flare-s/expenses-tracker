b42b18aeb5be3c66a7cc627cde31afc82f788ae7
flare-s <webflarez@gmail.com>
2022-08-05 17:54:23 +0100

    Added expenses state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/ExpensesForm";
 import Container from "./components/UI/Container";
 function App() {
+  const [expenses, setExpenses] = useState([]);
   return (
     <React.Fragment>
       <Header />
       <Container>
-        <Form />
+        <Form setExpenses={setExpenses} />
       </Container>
     </React.Fragment>
   );
--- src/components/ExpensesForm.js
+++ src/components/ExpensesForm.js
@@ -1,19 +1,23 @@
 import React from "react";
 import styles from "./ExpensesForm.module.css";
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles["form-group"]}>
-        <label id="exp-name">Expense name</label>
-        <input type="text" htmlFor="exp-name" name="exp-name" />
+        <label htmlFor="exp-name">Expense name</label>
+        <input required type="text" id="exp-name" name="exp-name" />
       </div>
       <div className={styles["form-group"]}>
-        <label id="exp-value">Expense value</label>
-        <input type="number" min="0" htmlFor="exp-value" name="exp-value" />
+        <label htmlFor="exp-value">Expense value</label>
+        <input required type="number" min="1" id="exp-value" name="exp-value" />
       </div>
       <div className={styles["form-group"]}>
-        <label id="exp-date">Expense date</label>
-        <input type="date" htmlFor="exp-date" name="exp-date" />
+        <label htmlFor="exp-date">Expense date</label>
+        <input required type="date" id="exp-date" name="exp-date" />
       </div>
       <div>
         <button className={styles["form-button"]} type="submit">
