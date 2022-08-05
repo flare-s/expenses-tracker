2821de183503488e8a997c42631821088a73d80a
flare-s <webflarez@gmail.com>
2022-08-05 16:42:29 +0100

    Added container component with styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/ExpensesForm";
+import Container from "./components/UI/Container";
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <Form />
+      <Container>
+        <Form />
+      </Container>
     </React.Fragment>
   );
 }
